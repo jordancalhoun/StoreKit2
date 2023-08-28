7348b4b436d11bfd0b1825f3dd69b55dfe499f96
Jordan Calhoun <jordancalhoun@gmail.com>
2023-08-28 10:41:10 -0400

    Initial Commit

---
+++ StoreKit2/StoreKit2App.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit2App.swift
+//  StoreKit2
+//
+//  Created by Jordan Calhoun on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
